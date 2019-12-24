3bde052493fa093334fdbe0d4b843842c98d3382
Kvasko Vladimir <Kvasko-V@yandex.ru>
2019-12-24 20:25:23 +0400

    реализовано временное сохранение принятых сообщений в JSON файле

--- App.js
+++ App.js
@@ -1,9 +1,33 @@
 const express = require("express");
 
+const fs = require("fs");
+const jsonfile = require('jsonfile');
+
 const app = express();
-app.get("/", function(request, response){
-     
-    response.send("<h2>Привет Express!</h2>");
+const jsonParser = express.json();
+
+const messagesFile = 'message.json';
+
+app.use(express.static("../client/whisper/build"));
+
+app.post("/messageReceive", jsonParser, function (request, response) {
+
+    jsonfile.readFile(messagesFile)
+        .then(
+            obj => {
+    
+                let messageList = obj;
+                messageList.push(request.body);
+                jsonfile.writeFile(messagesFile, messageList, function (err) {
+                    if (err) console.error(err)
+                });
+                response.json(messageList);
+            }
+        )
 });
 
 app.listen(3000);
+
+process.on("SIGINT", () => {
+    process.exit();
+});
