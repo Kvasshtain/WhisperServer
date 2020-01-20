935773ac45cc6ae513c22d624344fd392a2f8602
Kvasko Vladimir <Kvasko-V@yandex.ru>
2020-01-20 20:15:08 +0400

    Теперь статика (spa frontend react приложение) копируется после сборки в директорию static

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ mongoose.connect('mongodb://localhost:27017/whisperdb', { useNewUrlParser: true 
     });
 });
 
-app.use(express.static('../client/build'))
+app.use(express.static('./static/build'))
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
